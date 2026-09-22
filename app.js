d42507cf2dd108d44deeeb8ed84fdb9556a3ec69
Tung Huynh <137667812+acchtt@users.noreply.github.com>
2026-09-22 23:05:27 +0700

    Bump Aero × IVE version to 0.10.3

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ function renderTabTitle() {
   const author = cleanTabText(currentTabTrack.author);
 
   if (!title) {
-    document.title = 'Aero × IVE · v0.10.2';
+    document.title = 'Aero × IVE · v0.10.3';
     return;
   }
 
